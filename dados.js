747e6fe5cd0f8af8eb6e8fb5311580d1eedddd6b
Alan Silva <135849659+Alan-0718-sj@users.noreply.github.com>
2024-09-12 00:07:07 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -3,28 +3,28 @@ let dados = [
         titulo: "Matrix Reloaded",
         descricao: "Na sequência de Matrix, Neo e seus aliados continuam a luta contra as máquinas. Enquanto enfrentam os agentes e procuram por respostas, há momentos em que a habilidade de Neo de manipular o código da Matrix se torna crucial, culminando em sequências de ação visuais e espetaculares que desafiam as leis da física digital.",
         link: "http://en.wikipedia.org/wiki/The_Matrix_Reloaded",
-        tags: "neo trinity matrix 2003",
+        tags: "neo trinity matrix 2003 mr filme",
         video: "https://www.youtube.com/watch?v=0PxTAn4g20U"
     },
     {
         titulo: "Snowden",
         descricao: "Baseado em fatos reais, Snowden narra a história de Edward Snowden, um ex-contratado da CIA que revelou ao mundo um vasto programa de vigilância global. O filme explora a jornada de Snowden, desde sua iniciação nos segredos do governo dos Estados Unidos até sua decisão de se tornar um denunciante, e como ele utilizou seus conhecimentos em tecnologia para expor as atividades de espionagem.",
         link: "https://en.wikipedia.org/wiki/Snowden_(film)",
-        tags: "edward snowden governo espionagem",
+        tags: "edward snowden governo espionagem filme",
         video: "https://www.youtube.com/watch?v=QlSAiI3xMh4"
     },
     {
         titulo: "Oito Homens e um Segredo",
         descricao: "Embora não seja o foco principal do filme, há uma cena de destaque em 'Oito Homens e um Segredo' onde a personagem de Rihanna, uma hacker de gênio, demonstra suas habilidades para invadir um sistema de segurança altamente sofisticado. Essa sequência adiciona um elemento de suspense e ação à trama, mostrando a importância da tecnologia no mundo do crime.",
         link: "https://en.wikipedia.org/wiki/Ocean%27s_8",
-        tags: "sandra bullock anne hathaway rihanna",
+        tags: "sandra bullock anne hathaway rihanna filme",
         video: "https://www.youtube.com/watch?v=n5LoVcVsiSQ"
     },
     {
         titulo: "Quem sou eu - Nenhum sistema é seguro",
         descricao: "Este thriller alemão se concentra em um grupo de hackers que busca fama e poder, explorando as vulnerabilidades da internet e de grandes corporações. O filme acompanha um jovem hacker solitário que se junta a essa equipe e se vê envolvido em um jogo perigoso de gato e rato com as autoridades. A trama explora os dilemas morais e as consequências das ações dos hackers, além de apresentar um olhar realista sobre o mundo da cibersegurança.",
         link: "https://en.wikipedia.org/wiki/Who_Am_I_(2014_film)",
-        tags: "2014 russa benjamin max hanne",
+        tags: "2014 russa benjamin max hanne filme",
         video: "https://www.youtube.com/watch?v=5vnjheCqRIs"
     },
 ];
